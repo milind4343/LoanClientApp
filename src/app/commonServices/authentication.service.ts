f26f5dcc1b79c681a3a32f26697584fd98fa9c34
ChitraPatel <chitrapatel126@outlook.com>
2019-06-06 18:34:49 +0530

    commit

--- src/app/commonServices/authentication.service.ts
+++ src/app/commonServices/authentication.service.ts
@@ -1,22 +1,34 @@
 import { Injectable } from "@angular/core";
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Http, Headers } from "@angular/http";
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class AuthenticationService {
     
-    constructor(private router:Router) {
+    private staticDataUrl  = environment.domain + '/api/staticdata';    
+    private  token = 'Bearer'+' '+localStorage.getItem('jwt');
+    private headers = new Headers({ 'Content-Type': 'application/json', 'Authorization': this.token });
+
+    constructor(private router:Router, private http : Http) {
 
     }
 
     canActivate(): boolean {
         if(localStorage.getItem("jwt") === '' || localStorage.getItem("jwt") === null) {
-                return true;
+            return true;
         }
         else {  
             this.router.navigate(['dashboard']);
             return false;
-        }
-        
+        }        
+    }
+
+    getLoggedInUserDetail() : Observable<any> {
+        debugger;
+        return this.http.get(this.staticDataUrl + '/loggedinuser', { headers: this.headers })
+        .map(res => res.json());
     }
 
 }
