67406f18dd60721f9f1f0480cba4025ba95760e5
milind4343 <milind155@gmail.com>
2019-08-13 11:56:46 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,4 +5,6 @@
  */
 export const environment = {
   production: true,
+  domain:'http://ws-srv-net.in.webmyne.com/Applications/LoanManagement/LoanManagementAPI',
+  encryptkey : '123456$#@$^@1ERF'
 };
